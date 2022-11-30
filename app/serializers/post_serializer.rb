c3a884a1220131e0546c90bc22d9046e6c8a94eb
K-Irungu <110017552+K-Irungu@users.noreply.github.com>
2022-11-30 15:08:56 +0300

    commit tree b4b3d71786482e5ca2b19b401fa5ddd09473051e

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,9 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+  has_many :tags
+
+  # This method limits the post's content to 40 characters
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
+end
